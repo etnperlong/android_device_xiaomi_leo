a85bde74713fc656c4aa817bfdb8b5249f866494
Evans Mike <etnperlong@gmail.com>
2019-05-04 15:39:57 +0800

    leo: Inherit some common dotOS stuff

--- dot_leo.mk
+++ dot_leo.mk
@@ -6,8 +6,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 
-# Inherit some common MK stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+# Inherit some common dotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/leo/leo.mk)
